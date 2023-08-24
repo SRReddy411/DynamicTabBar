3ffdfeba10b476ac8976f46c31150bf8ad6c6c5b
RamiReddy GangiReddy <41564458+SRReddy411@users.noreply.github.com>
2023-08-24 18:49:03 +0530

    Update AppDelegate.swift
    scsd

--- DynamicTabBarItems/DynamicTabBarItems/AppDelegate.swift
+++ DynamicTabBarItems/DynamicTabBarItems/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-https://we.tl/t-LCySl8suOW
+// https://we.tl/t-LCySl8suOW
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
